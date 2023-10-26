18f1950c6baadc9dcaa6e46a49baaf41ccd8b7fa
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 14:39:22 -0400

    remove console.log

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector('.city').innerText = `Weather in ${name}`;
     document.querySelector(
       '.icon'
