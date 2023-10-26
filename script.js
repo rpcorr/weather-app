667c2af151f2fa4aa8c170e623ff3690e9b7596a
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 15:17:38 -0400

    apply math.trunc to temp output

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let weather = {
       '.icon'
     ).src = `https://openweathermap.org/img/wn/${icon}.png`;
     document.querySelector('.description').innerText = description;
-    document.querySelector('.temp').innerText = `${temp} °C`;
+    document.querySelector('.temp').innerText = `${Math.trunc(temp)} °C`;
     document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
     document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
     document.querySelector('.weather').classList.remove('loading');
