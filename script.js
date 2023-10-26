ad565425a53a6b47bb1b209e53ef39798e66d7e3
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 14:14:18 -0400

    display city weather info on page

--- script.js
+++ script.js
@@ -13,5 +13,13 @@ let weather = {
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector('.city').innerText = `Weather in ${name}`;
+    document.querySelector(
+      '.icon'
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = `${temp} °C`;
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+    document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
   },
 };
