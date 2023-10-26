a80a7793c5410b27ab4fc8745419fa35c6f0f3b1
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 13:59:37 -0400

    build displayWeather to gather data from json object and display to console

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let weather = {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
-  displayWeather: function (data) {},
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+  },
 };
