b5f0108fc1f467811ca051d64dce21a3464d68c7
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 13:51:10 -0400

    create a displayWeather function that pass in the data from fetchWeather

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let weather = {
   apiKey: 'a13ac26a83707dbc0a1f0a4bccf011f3',
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
   },
+  displayWeather: function (data) {},
 };
