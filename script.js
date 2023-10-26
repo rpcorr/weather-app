a25220b3bd5da3b18fa4b8d59f6440ea0ff885a1
Ronan Corr <ronan_corr@hotmail.com>
2023-10-26 13:46:53 -0400

    swap hard variables with dynamic variables

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let weather = {
-  apiKey: 'f1531ba5fe0b2f22181cd69d12f04f04',
+  apiKey: 'a13ac26a83707dbc0a1f0a4bccf011f3',
   fetchWeather: function () {
     fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=a13ac26a83707dbc0a1f0a4bccf011f3'
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
